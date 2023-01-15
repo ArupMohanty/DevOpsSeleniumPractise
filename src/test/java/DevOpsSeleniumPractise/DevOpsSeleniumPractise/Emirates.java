cd9acf488be88e94ad7768167bb47bc2529189d3
ArupMohanty <arup.kiit@gmail.com>
2023-01-15 20:18:30 +0000

    First Commit - Emirates Login Page

---
+++ src/test/java/DevOpsSeleniumPractise/DevOpsSeleniumPractise/Emirates.java
@@ -0,0 +1,30 @@
+package DevOpsSeleniumPractise.DevOpsSeleniumPractise;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Emirates {
+	@Test
+	public static void emiratesLoginPage() throws InterruptedException {
+		
+		//Below 2 lines code are to click on the Webpage notification (Push notifications)
+		//ChromeOptions options = new ChromeOptions();
+		//options.addArguments("--disable-notifications");
+		System.setProperty("webdriver.chrome.driver", "D:\\SeleniumWork\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.emirates.com/");
+		driver.manage().window().maximize();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//button[@id='onetrust-accept-btn-handler']")).click();
+		String expectedTitel = "Emirates | Fly Better";
+		String originalTitel = driver.getTitle();
+		Assert.assertEquals(expectedTitel, originalTitel);
+	}
+	
+	
+
+}
